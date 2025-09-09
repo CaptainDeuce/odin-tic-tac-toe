12817326b34daa6f3187caae3754623bbc4fde81
Ethan Boes <deuceboes@gmail.com>
2025-09-09 16:39:30 -0400

    Add the logic for checking if a player has won or if it's a tie

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -37,7 +37,7 @@ function Cell() {
     return {addMarker, getMarker};
 }
 
-function createPlayer () {
+function createPlayer() {
     const name = prompt("Please enter a name: ");
     const marker = prompt("Please enter an X or O: ");
 
@@ -65,6 +65,34 @@ const gameController = (function() {
         gameboard.placeMarker(row, column, getCurrentPlayer().marker);
 
         // check for a winner and handle that logic, such as a win message //
+        board = gameboard.getBoard();
+        switch(board) {
+            // the three rows winning
+            case board[0][0] === getCurrentPlayer().marker && board[0][1] === getCurrentPlayer().marker && board[0][2] === getCurrentPlayer().marker:
+            case board[1][0] === getCurrentPlayer().marker && board[1][1] === getCurrentPlayer().marker && board[1][2] === getCurrentPlayer().marker:
+            case board[2][0] === getCurrentPlayer().marker && board[2][1] === getCurrentPlayer().marker && board[2][2] === getCurrentPlayer().marker:
+
+            // the three columns winning
+            case board[0][0] === getCurrentPlayer().marker && board[1][0] === getCurrentPlayer().marker && board[2][0] === getCurrentPlayer().marker:
+            case board[0][1] === getCurrentPlayer().marker && board[1][1] === getCurrentPlayer().marker && board[2][1] === getCurrentPlayer().marker:
+            case board[0][2] === getCurrentPlayer().marker && board[1][2] === getCurrentPlayer().marker && board[2][2] === getCurrentPlayer().marker:
+
+            // the two diagonals winning
+            case board[0][0] === getCurrentPlayer().marker && board[1][1] === getCurrentPlayer().marker && board[2][2] === getCurrentPlayer().marker:
+            case board[0][2] === getCurrentPlayer().marker && board[1][1] === getCurrentPlayer().marker && board[2][0] === getCurrentPlayer().marker:
+                console.log(`${getCurrentPlayer().name} has won the game!`);
+                break;
+
+            // a tie
+            case board[0][0] === ("X" || "O") && board[0][1] === ("X" || "O") && board[0][2] === ("X" || "O") &&
+                board[1][0] === ("X" || "O") && board[1][1] === ("X" || "O") && board[1][2] === ("X" || "O") &&
+                board[2][0] === ("X" || "O") && board[2][1] === ("X" || "O") && board[2][2] === ("X" || "O"):
+                console.log("The game has ended in a tie.");
+                break;
+            default:
+        }
+
+
 
         switchPlayerTurn();
         printNewRound();
@@ -76,4 +104,4 @@ const gameController = (function() {
     return {playRound, getCurrentPlayer};
 })();
 
-// gameController.playRound(0, 1);
+gameController.playRound(0, 1);
